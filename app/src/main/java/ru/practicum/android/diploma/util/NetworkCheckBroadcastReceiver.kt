0de08114f78ab14b4592d5d641edbaaf16f4d684
novinnik <novinnik@yandex.ru>
2026-04-19 17:11:53 +0300

    Переписал Network в виду лимита по количеству элементов возврата

--- app/src/main/java/ru/practicum/android/diploma/util/NetworkCheckBroadcastReceiver.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/NetworkCheckBroadcastReceiver.kt
@@ -1,46 +1,34 @@
 package ru.practicum.android.diploma.util
 
+import android.Manifest
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
-import android.os.Build
 import android.widget.Toast
+import androidx.annotation.RequiresPermission
 import ru.practicum.android.diploma.R
 
 class NetworkCheckBroadcastReceiver : BroadcastReceiver() {
+    @RequiresPermission(Manifest.permission.ACCESS_NETWORK_STATE)
     override fun onReceive(context: Context?, intent: Intent?) {
-        if (intent?.action == ACTION_CONNECTIVITY) {
-            val isConnected = isNetworkConnected(context)
 
-            if (!isConnected) {
-                Toast.makeText(context, R.string.error_no_connection, Toast.LENGTH_LONG).show()
-            }
+        if (intent?.action != ConnectivityManager.CONNECTIVITY_ACTION) return
+
+        val connectivityManager = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val network = connectivityManager.activeNetwork
+
+        val activeNetwork = connectivityManager.getNetworkCapabilities(network)
+
+        val isConnected = activeNetwork?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true
+
+        if (!isConnected) {
+            Toast.makeText(context, R.string.error_no_connection, Toast.LENGTH_LONG).show()
         }
+
     }
 
-    private fun isNetworkConnected(context: Context?): Boolean {
-        val connectivityManager =
-            context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            val network = connectivityManager.activeNetwork ?: return false
-            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
-
-            return when {
-                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-                else -> false
-            }
-        } else {
-            val networkInfo = connectivityManager.activeNetworkInfo ?: return false
-            return networkInfo.isConnected
-        }
-    }
-
-    companion object {
-        const val ACTION_CONNECTIVITY = "android.net.conn.CONNECTIVITY_CHANGE"
-    }
 }

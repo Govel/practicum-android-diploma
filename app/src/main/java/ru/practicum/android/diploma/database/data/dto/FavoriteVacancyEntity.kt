82ba4a7f308b499c8c0d5c568c2a0029a937cb2c
Govel <s.gorlov123@gmail.com>
2026-04-22 01:29:48 +0300

    Исправлена Entity для БД

--- app/src/main/java/ru/practicum/android/diploma/database/data/dto/FavoriteVacancyEntity.kt
+++ app/src/main/java/ru/practicum/android/diploma/database/data/dto/FavoriteVacancyEntity.kt
@@ -3,34 +3,25 @@ package ru.practicum.android.diploma.database.data.dto
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
-import ru.practicum.android.diploma.vacancy.data.dto.Address
-import ru.practicum.android.diploma.vacancy.data.dto.Contacts
-import ru.practicum.android.diploma.vacancy.data.dto.Employer
-import ru.practicum.android.diploma.vacancy.data.dto.Employment
-import ru.practicum.android.diploma.vacancy.data.dto.Experience
-import ru.practicum.android.diploma.vacancy.data.dto.FilterArea
-import ru.practicum.android.diploma.vacancy.data.dto.FilterIndustry
-import ru.practicum.android.diploma.vacancy.data.dto.Salary
-import ru.practicum.android.diploma.vacancy.data.dto.Schedule
 
 @Entity(
     tableName = "favorite_vacancy_table",
-    indices = [Index(value = ["vacancyId"], unique = true)]
+    indices = [Index(value = ["id"], unique = true)]
 )
 data class FavoriteVacancyEntity(
     @PrimaryKey
     val id: Long,
     val name: String,
-    val salary: Salary?,
-    val address: Address?,
-    val experience: Experience?,
-    val schedule: Schedule?,
-    val employment: Employment?,
-    val contacts: Contacts?,
+    val salary: String?,
+    val address: String?,
+    val experience: String?,
+    val schedule: String?,
+    val employment: String?,
+    val contacts: String?,
     val description: String,
-    val employer: Employer,
-    val area: FilterArea,
-    val skills: List<String>,
+    val employer: String,
+    val area: String,
+    val skills: String,
     val url: String,
-    val industry: FilterIndustry
+    val industry: String
 )

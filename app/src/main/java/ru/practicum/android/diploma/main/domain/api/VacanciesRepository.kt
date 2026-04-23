4aa6c48dfcb2be844d521b161e26c819df155c25
Govel <s.gorlov123@gmail.com>
2026-04-23 15:55:21 +0300

    Изменено: - строка зарплаты строится корректно - domain-слой не зависит от data - порядок импортов

--- app/src/main/java/ru/practicum/android/diploma/main/domain/api/VacanciesRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/main/domain/api/VacanciesRepository.kt
@@ -1,9 +1,9 @@
 package ru.practicum.android.diploma.main.domain.api
 
 import kotlinx.coroutines.flow.Flow
-import ru.practicum.android.diploma.main.domain.models.VacancyFilter
 import ru.practicum.android.diploma.main.domain.models.Resource
 import ru.practicum.android.diploma.main.domain.models.VacancyCard
+import ru.practicum.android.diploma.main.domain.models.VacancyFilter
 
 interface VacanciesRepository {
     fun searchVacancies(expression: VacancyFilter): Flow<Resource<List<VacancyCard>?>>

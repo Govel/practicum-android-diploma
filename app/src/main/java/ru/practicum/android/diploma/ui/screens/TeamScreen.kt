e2d7b5c5799ac5098df1ce6954673a05bab5fc44
novinnik <novinnik@yandex.ru>
2026-04-24 01:03:49 +0300

    Создал верхний TopBar Добавил изображение для экрана "Команда" Сверстал экран "Команда"

--- app/src/main/java/ru/practicum/android/diploma/ui/screens/TeamScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/screens/TeamScreen.kt
@@ -1,27 +1,74 @@
 package ru.practicum.android.diploma.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.ui.navigation.AppBarTop
 
 @Composable
 fun TeamScreen() {
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.onPrimary),
-        contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = stringResource(R.string.team),
-            style = MaterialTheme.typography.titleLarge
-        )
+        AppBarTop(title = stringResource(R.string.team))
+
+        Spacer(modifier = Modifier.padding(top = 8.dp))
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 16.dp, horizontal = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(R.string.worked_on_app),
+                style = MaterialTheme.typography.displayLarge
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            ItemDeveloper(name = stringResource(R.string.anastasia_yakovleva))
+
+            ItemDeveloper(name = stringResource(R.string.sergey_gorlov))
+
+            ItemDeveloper(name = stringResource(R.string.ivan_novozhilov))
+
+            Image(
+                painter = painterResource(R.drawable.cat),
+                contentDescription = stringResource(R.string.team),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize().padding(top = 8.dp)
+            )
+        }
     }
+}
+
+@Composable
+private fun ItemDeveloper(
+    name: String
+) {
+    Text(
+        text = name,
+        style = MaterialTheme.typography.bodyMedium,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp)
+    )
 }

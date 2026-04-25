ea4a8535ad0f371fd24c44bf9bdb270764f6c11f
Nastya <anastasiasizonenkoo@yandex.ru>
2026-04-25 18:39:25 +0300

    detekt

--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -5,7 +5,7 @@ import ru.practicum.android.diploma.main.data.impl.VacancyRepositoryImpl
 import ru.practicum.android.diploma.main.domain.api.VacanciesRepository
 
 val repositoryModule = module {
-    factory <VacanciesRepository> {
+    factory<VacanciesRepository> {
         VacancyRepositoryImpl(get())
     }
 }

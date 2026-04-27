98dd549fda70a369410b5c5d5ed2e8015a7c596e
novinnik <novinnik@yandex.ru>
2026-04-27 22:33:37 +0300

    изменен класс импорта

--- app/src/main/java/ru/practicum/android/diploma/ui/screens/favorites/FavoritesScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/screens/favorites/FavoritesScreen.kt
@@ -37,7 +37,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
+import coil3.compose.AsyncImage
 import coil3.request.ImageRequest
 import coil3.request.crossfade
 import org.koin.androidx.compose.koinViewModel

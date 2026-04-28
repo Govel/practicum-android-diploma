8acd042794ed1f5cd13958f4e31ee974d3d1970e
novinnik <novinnik@yandex.ru>
2026-04-28 19:08:21 +0300

    исправление ошибки gridle после внесение изменений по размещению классов

--- app/src/main/java/ru/practicum/android/diploma/ui/navigation/ProjectNavHost.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/navigation/ProjectNavHost.kt
@@ -6,10 +6,10 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import ru.practicum.android.diploma.main.ui.screen.SearchScreen
-import ru.practicum.android.diploma.ui.screens.favorites.FavoritesScreen
-import ru.practicum.android.diploma.ui.screens.team.TeamScreen
 import ru.practicum.android.diploma.ui.screens.VacancyDetailScreen
+import ru.practicum.android.diploma.ui.screens.favorites.FavoritesScreen
 import ru.practicum.android.diploma.ui.screens.filter.FilterScreen
+import ru.practicum.android.diploma.ui.screens.team.TeamScreen
 import ru.practicum.android.diploma.ui.screens.vacancy
 
 @Composable

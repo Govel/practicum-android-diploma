381a228768c6047c112b2b01b28017fb3559b565
Nastya <anastasiasizonenkoo@yandex.ru>
2026-04-29 18:11:58 +0300

    fix

--- app/src/main/java/ru/practicum/android/diploma/ui/navigation/BottomNavBar.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/navigation/BottomNavBar.kt
@@ -22,13 +22,14 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 
+private val navItems = ScreenNavItem.items
+
 @Composable
 fun BottomNavigationBar(
     navController: NavHostController,
     currentRoute: String?
-) {
-    val items = ScreenNavItem.items
 
+) {
     val colorsNavigation = NavigationBarItemDefaults.colors(
         selectedIconColor = MaterialTheme.colorScheme.primary,
         selectedTextColor = MaterialTheme.colorScheme.primary,
@@ -48,7 +49,7 @@ fun BottomNavigationBar(
             containerColor = MaterialTheme.colorScheme.surface,
             tonalElevation = 0.dp
         ) {
-            items.forEach { item ->
+            navItems.forEach { item ->
                 val isSelected = currentRoute == item.route
                 NavigationBarItem(
                     selected = isSelected,
